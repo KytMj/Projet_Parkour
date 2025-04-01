a7b438210611354049cdbf417cbab989b2f26cf8
Cassandre9610 <cassandre.yontailleux@gmail.com>
2025-04-01 20:08:35 +0200

    Design og inscriptionPage

--- app/src/main/java/com/example/projet_parkour/view/InscriptionCompetitorsPage.kt
+++ app/src/main/java/com/example/projet_parkour/view/InscriptionCompetitorsPage.kt
@@ -6,12 +6,17 @@ import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -30,8 +35,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.projet_parkour.api.NetworkResponse
 import com.example.projet_parkour.model.CompetitorModel
@@ -69,17 +77,30 @@ fun InscriptionCompetitorsPage(
         .fillMaxSize()
         .background(MaterialTheme.colorScheme.background)){
         Column {
-            AddToListRegisteredCompetitors(viewModel, registeredCompetitorsList)
-            PotentialsCompetitorsPage(modifier, viewModel, ageMin, ageMax, gender, selectedText, registeredCompetitorsList)
-            if (selectedText.value != "") {
-                idAddCompetitor.value = selectedText.value.split(".").get(0).substring(3).toInt()
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp).border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.secondary,
+                        shape = RoundedCornerShape(10.dp)
+                    ).padding(10.dp)
+            ){
+                AddToListRegisteredCompetitors(viewModel, registeredCompetitorsList)
+                PotentialsCompetitorsPage(modifier, viewModel, ageMin, ageMax, gender, selectedText, registeredCompetitorsList)
+                if (selectedText.value != "") {
+                    idAddCompetitor.value = selectedText.value.split(".").get(0).substring(3).toInt()
+                }
+                Button(onClick = {
+                    RegisterCompetitorOnClick(idAddCompetitor.intValue, competitionId, context, viewModel, addCompetitorResult)
+                }) {
+                    Text("Inscrire un participant")
+                }
             }
-            Button(onClick = {
-                RegisterCompetitorOnClick(idAddCompetitor.intValue, competitionId, context, viewModel, addCompetitorResult)
-            }) {
-                Text("Inscrire un participant")
-            }
+
             RegisteredCompetitorsPage(modifier, viewModel, competitionId)
+
         }
         FloatingActionButton(
             modifier = Modifier
@@ -156,34 +177,56 @@ fun RegisteredCompetitorsPage(
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(modifier = Modifier.padding(bottom = 10.dp), text ="Participants inscrits à la compétition")
-        when(val result = competitorByCompetitionResult.value){
-            is NetworkResponse.Error -> {
-                Text(text = result.message)
-            }
+        Text(text ="Participants inscrits à la compétition",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(top = 5.dp, bottom = 15.dp),
+            fontWeight = FontWeight.Bold)
 
-            is NetworkResponse.Loading -> {
-                CircularProgressIndicator()
-            }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(10.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colorScheme.secondary)
+                .padding(top=20.dp,start = 10.dp, end = 10.dp, bottom = 20.dp)
+        ) {
 
-            is NetworkResponse.Success -> {
-                LazyColumn {
-                    items(result.data.size){ index ->
-                        val data = result.data[index]
-                        Card(
-                            colors = CardDefaults.cardColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer
-                            ),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 10.dp, end = 10.dp, bottom = 5.dp),
-                        ) {
-                            Text(modifier = Modifier.padding(10.dp), text = data.last_name.uppercase() + " " + data.first_name + data.gender + data.born_at)
+            when (val result = competitorByCompetitionResult.value) {
+                is NetworkResponse.Error -> {
+                    Text(text = result.message)
+                }
+
+                is NetworkResponse.Loading -> {
+                    CircularProgressIndicator()
+                }
+
+                is NetworkResponse.Success -> {
+                    LazyColumn {
+                        items(result.data.size) { index ->
+                            val data = result.data[index]
+                            Card(
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.primary
+                                ),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(start = 10.dp, end = 10.dp, bottom = 5.dp),
+                            ) {
+                                Row (modifier = Modifier.fillMaxWidth().padding(10.dp),
+                                    horizontalArrangement = Arrangement.SpaceBetween){
+                                    Text(text = data.last_name.uppercase()+" "+data.first_name, fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                                    Text(data.born_at+"   "+data.gender)
+                                }
+
+                            }
                         }
                     }
                 }
+
+                null -> {}
             }
-            null -> {}
         }
     }
 }
